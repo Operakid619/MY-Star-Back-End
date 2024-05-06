88e2280192f21c51453fea7b8c9db64c35023505
Abdulquddus Nuhu <abdulquddusnuhu@gmail.com>
2024-05-06 17:11:46 +0100

    fix edit busdriver

--- src/Shared/Models/Requests/EditBusdriverRequest.cs
+++ src/Shared/Models/Requests/EditBusdriverRequest.cs
@@ -12,6 +12,6 @@ namespace Shared.Models.Requests
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
-        public Guid? BusId { get; set; }
+        //public Guid? BusId { get; set; }
     }
 }
